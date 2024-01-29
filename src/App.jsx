6f1c081e103bf5def78bd76a91eac594c52f1b3d
seba-orellana <seba_orellana1@outlook.com.ar>
2024-01-29 19:53:04 -0300

    feat: added search bar and improved code

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,51 @@ import cloudyNight from './assets/cloudy-night.svg'
 import night from './assets/night.svg'
 import rainy from './assets/rainy.svg'
 import thunder from './assets/thunder.svg'
+import errorImg from './assets/error.svg'
 
 import axios from 'axios'
 import './App.css'
 
 function App() {
 
-  const weather = 'https://www.meteosource.com/api/v1/free/point?place_id=rio-cuarto&sections=current&language=en&units=metric&key=x8ckzortw8q1zliipgfqfolmsupk8i2i23j41m4o'
-
+  const [city, setCity] = useState('')
   const [data, setData] = useState({})
+  const [name, setName] = useState('')
+  const [inputValue, setInputValue] = useState('')
 
-  useEffect(() => {
-    checkData()
-    })
+  const weather = `https://www.meteosource.com/api/v1/free/point?place_id=${city}&sections=current&language=en&units=metric&key=x8ckzortw8q1zliipgfqfolmsupk8i2i23j41m4o`
 
-  const checkData = () => {
-    axios.get(weather).then((response => {
-      setData(response.data)
-    } ))
-  };
+  const checkData = (event) => {
+    if (event) {
+      axios.get(weather).then((response => {
+        setName(inputValue)
+        setData(response.data)
+        setInputValue('')
+      } )).catch((error => {
+        setData(error.code)
+      }))
+    }
+  }
+
+  const modifCity = (event) => {
+    setCity(event.target.value.replace(/\s+/g, "-").toLowerCase())
+    setInputValue(event.target.value)
+  }
 
   return (
     <>
+
+    <div className="header">
+      <input className='searchBar' placeholder='Ingrese la ciudad' type='text' value={ inputValue } 
+      onChange={ modifCity }></input>
+    </div>
+
     <div className="imagen">
-      { data.current ?
+      { (data.current) ?
         [2, 3].includes(data.current.icon_num) ? <img src={ day } alt="imagen_dia" /> :
         [7, 8, 9].includes(data.current.icon_num) ? <img src={ cloudy } alt="imagen_nubes" /> : 
         [10, 11, 12, 13].includes(data.current.icon_num) ? <img src={ rainy } alt="imagen_lluvia" /> :
         [26, 27, 28].includes(data.current.icon_num) ? <img src={ night } alt="imagen_noche" /> :
-        [14, 15, 33].includes(data.current.icon_num) ? <img src={ thunder } alt="imagen_tormenta" /> :
         [14, 15, 33].includes(data.current.icon_num) ? <img src={ thunder } alt="imagen_tormenta" /> :
         [4, 5, 6].includes(data.current.icon_num) ? <img src={ cloudyDay } alt="imagen_dia_nublado" /> :
         [29, 30, 31].includes(data.current.icon_num) ? <img src={ cloudyNight } alt="imagen_noche_nublado" /> :
@@ -45,33 +61,36 @@ function App() {
     </div>
 
     <div className="container">
-      { data.current ? <p className="temp">{ data.current.temperature.toFixed()} °C</p> : null }
-      <p className="ciudad">Rio Cuarto</p>
+      { data.current ?
+      <>
+      <p className="temp">{ data.current.temperature.toFixed()} °C</p>
+      <p className="ciudad"> { name }</p>
+      </> : null }
     </div>
 
     <div className="sub-container">
       <div className="box">
-        { data.current? <p className='value'>{data.current.cloud_cover} %</p> : null }
+        { data.current ?
+        <> <p className='value'>{data.current.cloud_cover} %</p>
         <p className='text'>Cielo cubierto</p>
+        </> : null }
       </div>
 
       <div className="box">
-        { data.current? <p className='value'>{data.current.precipitation.total}</p> : null }
-        <p className='text'>Precip. Acum</p>
-      </div>
-
-      <div className="box">
-        { data.current ? <p className="value">{ data.current.wind.speed} m/s</p> : null }
+        { data.current ?
+        <>
+        <p className="value">{ data.current.wind.speed} m/s</p>
         <p className='text'>Viento</p>
+        </> : null }
       </div>
     </div>
 
     <div className="consultar">
-      <button onClick={ checkData }>Consultar</button>
+      <button onClick= { checkData }>Consultar</button>
     </div>
 
     <section className='created'>
-      <h3>Creado por Sebastian Orellana</h3>
+      <h3>seba-orellana</h3>
     </section>
     </>
   )
